d29e35fe04484d7d2674901cdf1294b66e56e72d
Richard Memory <richardfmemory@gmail.com>
2018-09-17 21:35:22 -0600

    Complete initial styling

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,16 +1,26 @@
 /* By using Link instead of a regular anchor link, it means
  * the link is bound to HTML5 push state */
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-	<div>
-		<Link href="/">
-			<a>Go home!</a>
+	<NavStyles>
+		<Link href="/items">
+			<a>Items</a>
 		</Link>
 		<Link href="/sell">
-			<a>Sell!</a>
+			<a>Sell</a>
 		</Link>
-	</div>
+		<Link href="/signup">
+			<a>Signup</a>
+		</Link>
+		<Link href="/orders">
+			<a>Orders</a>
+		</Link>
+		<Link href="/me">
+			<a>Account</a>
+		</Link>
+	</NavStyles>
 )
 
 export default Nav;
