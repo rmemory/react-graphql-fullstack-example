d1ced4916ec3c0879ecdb25ba046762fc4563c36
Richard Memory <richardfmemory@gmail.com>
2018-09-19 19:31:24 -0600

    Add simple example of GraphQL Yoga

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-// lets go!
+// lets go, start GraphQL Yoga (Node) server
+
+require('dotenv').config({ path: 'variables.env'});
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO use express middleware to handle cookies for JWT
+// TODO use express middleware to populate current user
+
+server.start({
+	cors: {
+		credentials: true,
+		origin: process.env.FRONTEND_URL,
+	},
+}, deets => {
+	console.log(`Server is now running on port 
+	http:/localhost:${deets.port}`);
+}
+
+);
