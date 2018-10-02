98612832f9ef664028aad4bacc078e5430fbca94
Richard Memory <richardfmemory@gmail.com>
2018-10-02 14:32:56 -0600

    Add initial theme in Page

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Header from './Header';
 import Meta from './Meta';
 
-const MyButton = styled.button`
-	background: red;
-	font-size: ${props => props.huge ? '100px' : '50px'};
+const theme = {
+	red: '#FF0000',
+	black: '#393939',
+	grey: '#3A3A3A',
+	lightgrey: '#E1E1E1',
+	offWhite: '#EDEDED',
+	maxWidth: '1000px',
+	bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
 
-	span {
-		font-size: 100px;
-	}
+const PageDiv = styled.div`
+	background: white;
+	color: ${props => props.theme.black};
+`;
 
-	.medium-arrow {
-		font-size: 75px;
-	}
+const ComponentDiv = styled.div`
+	max-width: ${props => props.theme.maxWidth};
+	margin: 0 auto;
+	padding: 2rem;
 `;
 
 /* Page wraps all other Components. See pages/_app.js. */
 class Page extends Component {
 	render() {
 		return (
-			<div>
-				<Meta />
-				<Header />
-				<MyButton>
-					Click Me
-					<span>-></span>
-				</MyButton>
+			<ThemeProvider theme={theme}>
+				<PageDiv>
+					<Meta />
+					<Header />
 
-				<MyButton huge>
-					Click Me
-					<span className="medium-arrow">-></span>
-				</MyButton>
-				{this.props.children}
-			</div>
+					{/* All components (other than those above like Meta, Header) are
+					rendered inside of InnerDiv */}
+					<ComponentDiv>
+						{this.props.children}
+					</ComponentDiv>
+				</PageDiv>
+			</ThemeProvider>
 		);
 	}
 }
