1e8c57d26a30ce030d3311c2064103aba46b26aa
Richard Memory <richardfmemory@gmail.com>
2018-10-02 14:43:25 -0600

    Add Header styling

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,9 +1,57 @@
 import Nav from './Nav';
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const HeaderDiv = styled.div`
+	.bar {
+		border-bottom: 10px solid ${props => props.theme.black};
+		display: grid;
+		grid-template-columns: auto 1fr;
+		justify-content: space-between;
+		align-items: stretch;
+		
+		@media (max-width: 1300px) {
+			grid-template-columns: 1fr;
+			justify-content: center;
+		}
+	}
+
+	.sub-bar {
+		display: grid;
+		grid-template-columns: 1fr auto;
+		border-bottom: 1px solid ${props => props.theme.lightgrey};
+	}
+`;
+
+const LogoH1 = styled.h1`
+	font-size: 4rem;
+	margin-left: 2rem;
+	position: relative;
+	z-index: 2;
+	transform: skew(-7deg);
+
+	a {
+		padding: 0.5rem 1rem;
+		background: ${props => props.theme.red};
+		color: white;
+		text-transform: uppercase;
+		text-decoration: none;
+	}
+
+	@media (max-width: 1300px) {
+		margin: 0;
+		text-align: center;
+	}
+`;
 
 const Header = () => (
-	<div>
+	<HeaderDiv>
 		<div className="bar">
-			<a href="">Sick Fits</a>
+			<LogoH1>
+				<Link href="/">
+					<a>Sick Fits</a>
+				</Link>
+			</LogoH1>
 			<Nav />
 		</div>
 		<div className="sub-bar">
@@ -12,7 +60,7 @@ const Header = () => (
 		<div>
 			Cart
 		</div>
-	</div>
+	</HeaderDiv>
 );
 
 export default Header;
