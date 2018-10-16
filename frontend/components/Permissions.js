624bc045e1b2bfe16918a8a938158ebf3ee56bca
Richard Memory <richardfmemory@gmail.com>
2018-10-16 13:55:21 -0600

    Fix permissions table style so box can be selected when clicking the label

--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -4,6 +4,7 @@ import gql from 'graphql-tag';
 import Error from './ErrorMessage';
 import Table from './styles/Table';
 import SickButton from './styles/SickButton';
+import PropTypes from  'prop-types';
 
 const possiblePermissions = [
 	'ADMIN',
@@ -38,12 +39,12 @@ class Permissions extends Component {
 									<th>Name</th>
 									<th>Email</th>
 									{possiblePermissions.map(permission => 
-										<th>{permission}</th>)}
+										 <th key={permission}>{permission}</th>)}
 									<th>Update</th>
 								</tr>
 							</thead>
 							<tbody>
-								{data.users.map(user => <User user={user} />)}
+								{data.users.map(user => <UserPermissions key={user.id} user={user} />)}
 							</tbody>
 						</Table>
 					</div>
@@ -53,7 +54,34 @@ class Permissions extends Component {
 	}
 }
 
-class User extends React.Component {
+class UserPermissions extends React.Component {
+	static propTypes = {
+		user: PropTypes.shape({
+			name: PropTypes.string,
+			email: PropTypes.string,
+			id: PropTypes.string,
+			permissions: PropTypes.array,
+		}).isRequired,
+	};
+
+	state = {
+		// Initial seed only
+		permissions: this.props.user.permissions,
+	}
+
+	handlePermissionChange = (event) => {
+		const checkbox = event.target;
+		let updatePermissions = [...this.state.permissions];
+
+		if (checkbox.checked) {
+			updatePermissions.push(checkbox.value);
+		} else {
+			updatePermissions = updatePermissions.filter(permission => permission != checkbox.value);
+		}
+
+		this.setState({permissions: updatePermissions});
+	};
+
 	render() {
 		const user = this.props.user;
 		return (
@@ -61,9 +89,14 @@ class User extends React.Component {
 				<td>{user.name}</td>
 				<td>{user.email}</td>
 				{possiblePermissions.map(permission => (
-					<td>
+					<td key={permission} >
 						<label htmlFor={`${user.id}-permission-${permission}`}>
-							<input type="checkbox"/>
+							<input id={`${user.id}-permission-${permission}`}
+								type="checkbox" 
+								checked={this.state.permissions.includes(permission)}
+								value={permission}
+								onChange={this.handlePermissionChange}
+								/>
 						</label>
 					</td>
 				))}
